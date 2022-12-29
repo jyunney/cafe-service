676c0e8140c1c4d984dcc8bb64af66ec70638c87
janghocheol <jang806@daum.net>
2022-12-29 04:44:34 +0000

    add to cafe order Policy 구현

--- cafe/src/main/java/cafe/service/domain/CafeOrder.java
+++ cafe/src/main/java/cafe/service/domain/CafeOrder.java
@@ -1,149 +1,112 @@
 package cafe.service.domain;
 
-import cafe.service.domain.MakeStarted;
-import cafe.service.domain.OrderApproved;
-import cafe.service.domain.MakeCompleted;
-import cafe.service.domain.OrderCanceled;
-import cafe.service.CafeApplication;
-import javax.persistence.*;
-import java.util.List;
-import lombok.Data;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PostPersist;
+import javax.persistence.Table;
+
+import cafe.service.CafeApplication;
+import lombok.Data;
 
 @Entity
-@Table(name="CafeOrder_table")
+@Table(name = "CafeOrder_table")
 @Data
 
-public class CafeOrder  {
+public class CafeOrder {
 
-
-    
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    
-    
-    
-    
-    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long cafeOrderId;
-    
-    
-    
-    
-    
+
     private Long customerId;
-    
-    
-    
-    
-    
+
     private Long cafeId;
-    
-    
-    
-    
-    
+
     private Long menuId;
-    
-    
-    
-    
-    
+
     private Integer qty;
-    
-    
-    
-    
-    
+
     private Date orderDate;
-    
-    
-    
-    
-    
+
     private String status;
-    
-    
-    
-    
-    
+
     private Long orderId;
 
     @PostPersist
-    public void onPostPersist(){
-
+    public void onPostPersist() {
 
         MakeStarted makeStarted = new MakeStarted(this);
         makeStarted.publishAfterCommit();
 
-
-
         OrderApproved orderApproved = new OrderApproved(this);
         orderApproved.publishAfterCommit();
 
-
-
         MakeCompleted makeCompleted = new MakeCompleted(this);
         makeCompleted.publishAfterCommit();
-
-
 
         OrderCanceled orderCanceled = new OrderCanceled(this);
         orderCanceled.publishAfterCommit();
 
     }
 
-    public static CafeOrderRepository repository(){
+    public static CafeOrderRepository repository() {
         CafeOrderRepository cafeOrderRepository = CafeApplication.applicationContext.getBean(CafeOrderRepository.class);
         return cafeOrderRepository;
     }
 
+    public static void addToCafeOrder(Paid paid) {
 
-
-
-    public static void addToCafeOrder(Paid paid){
-
-        /** Example 1:  new item 
+        /** Example 1: new item **/
         CafeOrder cafeOrder = new CafeOrder();
-        repository().save(cafeOrder);
+        cafeOrder.setCustomerId(paid.getCustomerId());
+        cafeOrder.setCafeId(paid.getCafeId());
+        cafeOrder.setMenuId(paid.getMenuId());
+        cafeOrder.setQty(paid.getQty());
+        cafeOrder.setOrderDate(paid.getOrderDate());
+        cafeOrder.setStatus("paid");
+        cafeOrder.setOrderId(paid.getOrderId());
+        cafeOrder.repository().save(cafeOrder);
 
-        */
+        /**
+         * Example 2: finding and process
+         * 
+         * repository().findById(paid.get???()).ifPresent(cafeOrder->{
+         * 
+         * cafeOrder // do something
+         * repository().save(cafeOrder);
+         * 
+         * 
+         * });
+         */
 
-        /** Example 2:  finding and process
-        
-        repository().findById(paid.get???()).ifPresent(cafeOrder->{
-            
-            cafeOrder // do something
-            repository().save(cafeOrder);
-
-
-         });
-        */
-
-        
-    }
-    public static void changeCafeOrder(PaymentCanceled paymentCanceled){
-
-        /** Example 1:  new item 
-        CafeOrder cafeOrder = new CafeOrder();
-        repository().save(cafeOrder);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(paymentCanceled.get???()).ifPresent(cafeOrder->{
-            
-            cafeOrder // do something
-            repository().save(cafeOrder);
-
-
-         });
-        */
-
-        
     }
 
+    public static void changeCafeOrder(PaymentCanceled paymentCanceled) {
+
+        /**
+         * Example 1: new item
+         * CafeOrder cafeOrder = new CafeOrder();
+         * repository().save(cafeOrder);
+         * 
+         */
+
+        /**
+         * Example 2: finding and process
+         * 
+         * repository().findById(paymentCanceled.get???()).ifPresent(cafeOrder->{
+         * 
+         * cafeOrder // do something
+         * repository().save(cafeOrder);
+         * 
+         * 
+         * });
+         */
+
+    }
 
 }
